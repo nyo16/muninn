837cb7e72fca2a8ed9f3f9f619bd6c4328889907
Niko Maroulis <nik.maroulis@gmail.com>
2026-06-13 12:23:21 -0400

    Update checksums 0.5.5 (#28)

--- checksum-Elixir.Muninn.Native.exs
+++ checksum-Elixir.Muninn.Native.exs
@@ -1,20 +1,38 @@
 %{
-  "libmuninn-v0.5.4-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a6b77d8c7893a7196d236075cfcae2127bcfbf472b9a803a1a9b9b389a39b0c1",
-  "libmuninn-v0.5.4-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:59bd3b401035fbee4db02ed22e6852b2f137a238981b86e8841005d6597a09c1",
-  "libmuninn-v0.5.4-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:9591caeb5955611f77395827e32cf8bdcfc56432d1def7488dafe02437f2437a",
-  "libmuninn-v0.5.4-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:5513e7a531805502fbfb430207c57820334a37e3a4fb10f070b0dd5d27117e2f",
-  "libmuninn-v0.5.4-nif-2.15-x86_64-unknown-linux-gnu--legacy_cpu.so.tar.gz" => "sha256:f55144df8c1fce4df43ff10b610107128cca8d0a4778e94b91ea1640c7e021eb",
-  "libmuninn-v0.5.4-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:c1cec07fe07acad716a2ec6474c1ce7df716176cf07c513417e5c8f32ca7532d",
-  "libmuninn-v0.5.4-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:48a5e30ce0e218ffa7ea6234c833425af815f0d18798c97387f4250ddd34b84e",
-  "libmuninn-v0.5.4-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:5e9021fe35e5299dcc2edbe07af37e1b1e40a9fab8ca32ae28435b69900cc4a6",
-  "libmuninn-v0.5.4-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:dc64800425e4624205600995ceb0d4731753c2f988e9f75d1c5941949cab7917",
-  "libmuninn-v0.5.4-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:ee8fb75d9876b6963e8238e74f3dc18a4d4b375949e65d503661e6f0eeb821ad",
-  "libmuninn-v0.5.4-nif-2.16-x86_64-apple-darwin.so.tar.gz" => "sha256:0d506e81e7693680730d531957428551b3177d8115c117c303176724db914934",
-  "libmuninn-v0.5.4-nif-2.16-x86_64-unknown-linux-gnu--legacy_cpu.so.tar.gz" => "sha256:0d3d87d8f3997872c3c05ce92195511b65bf66c52a50f1a4f726e7e641a2afe0",
-  "libmuninn-v0.5.4-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:2f5e7febade69233f976c439e0652a4534c86509afa109e0edf3af937e66e61d",
-  "libmuninn-v0.5.4-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:12948b7f6446a95db1e3ce247886b118ea4ea38dfca82213ab4832b8b25f2dea",
-  "muninn-v0.5.4-nif-2.15-x86_64-pc-windows-gnu--legacy_cpu.dll.tar.gz" => "sha256:7eb52abe0c9c5fa667ec771f6f0682a6ff7ed267aeb577b4fb45fa19bf50dc56",
-  "muninn-v0.5.4-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:2c45f66e1666d6ee3babcfbd5d14397f5be14ad6595be01f6262c784bc25c363",
-  "muninn-v0.5.4-nif-2.16-x86_64-pc-windows-gnu--legacy_cpu.dll.tar.gz" => "sha256:3f21a640a90aaab6a33f088ff378d1cb52ab9e43a40329b4c433c414c0283cf7",
-  "muninn-v0.5.4-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:185af7c5630bb165e5a0c53419705f0e8b0d267b5eb3897a7c0c9341e1d18046",
+  "libmuninn-v0.5.5-nif-2.15-aarch64-apple-darwin.so.tar.gz" =>
+    "sha256:9995a94dc78e5040c5fc648d853a66797cca6c8691761679b0de01fb054ca4d1",
+  "libmuninn-v0.5.5-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:0a7311a515688bcbda8fca3a9e73a31cff68cbf7a2d12eda178d1af0f95e0f01",
+  "libmuninn-v0.5.5-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:0b09b1dba2592240c7545636158235ed4a879d229c65e883f34d7ae88d94c0ca",
+  "libmuninn-v0.5.5-nif-2.15-x86_64-apple-darwin.so.tar.gz" =>
+    "sha256:e3f83f0816741e9ff1f699b626dc2e4acc198958b591286b920468c02741ca57",
+  "libmuninn-v0.5.5-nif-2.15-x86_64-unknown-linux-gnu--legacy_cpu.so.tar.gz" =>
+    "sha256:dacec17a90430ad23cb1b3efa683b8993abfa9042aabc02c025f7b5d76acd3e7",
+  "libmuninn-v0.5.5-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:1312000d31852c40d302acfb61c27a2d6491fa2d17dbfb31e8f772c2330c70b6",
+  "libmuninn-v0.5.5-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:d602e3c7f358619ea3c9a029710405d7cb4c65cd3dfa174b7ff581b5baaf7dc2",
+  "libmuninn-v0.5.5-nif-2.16-aarch64-apple-darwin.so.tar.gz" =>
+    "sha256:8913670090d81c527dd58a2d49cc44714b515ca87ca53f3d1ed6670e503681e3",
+  "libmuninn-v0.5.5-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:f48da3da1e81d362a68612ef62f32b594ac00cb6a2de3fc801518540429f0f9b",
+  "libmuninn-v0.5.5-nif-2.16-aarch64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:05ab73241a1da9841c8d65d089d26d7f0dae2dbd91584d4fb84e043d116775d2",
+  "libmuninn-v0.5.5-nif-2.16-x86_64-apple-darwin.so.tar.gz" =>
+    "sha256:031926a751facc912b3615f294eedd5a43ed43e3e49fe55bea73aa63b985645b",
+  "libmuninn-v0.5.5-nif-2.16-x86_64-unknown-linux-gnu--legacy_cpu.so.tar.gz" =>
+    "sha256:edfaddd3dd0c597fbd83109a6c107451284565e9cc0c9b3cac9de800e02908a0",
+  "libmuninn-v0.5.5-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" =>
+    "sha256:c4fed16ebb1d58e7f241cee152bf7f1ad3f90eb6b4532118601ec13ada4f830d",
+  "libmuninn-v0.5.5-nif-2.16-x86_64-unknown-linux-musl.so.tar.gz" =>
+    "sha256:931539669b797d4804c271a9b28166521a31265d9eaa35e7c77601320cf0a8e3",
+  "muninn-v0.5.5-nif-2.15-x86_64-pc-windows-gnu--legacy_cpu.dll.tar.gz" =>
+    "sha256:721c13675ccdeca08f61ae8276af9a851a5a043376197f874544d8a1ff39b7d4",
+  "muninn-v0.5.5-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" =>
+    "sha256:daed62d49121053f1f039561b673f1f27bad49be98444382e5d9d569f0d21b31",
+  "muninn-v0.5.5-nif-2.16-x86_64-pc-windows-gnu--legacy_cpu.dll.tar.gz" =>
+    "sha256:1055246bca71b832f121918f478666d5fb9b709f98801bb75ad7a36638d7d371",
+  "muninn-v0.5.5-nif-2.16-x86_64-pc-windows-gnu.dll.tar.gz" =>
+    "sha256:aabef9e6e9f604140b3dcbbcd9c35ba479573f26c0c6f3a781a1ffbe7569d8f0"
 }
